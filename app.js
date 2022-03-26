676a26b7f4766b7fdabe9e12af8e05db757ba7da
Hashmatullah Nazari <hn0153@gmail.com>
2022-03-25 21:13:42 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var element = document.getElementById("element");
+  element.style.borderBottomRightRadius = "25px";
+}
+  
+  function myFunction1() {
+  var element = document.getElementById("element");
+  element.style.borderBottomRightRadius = "255px";
+}
